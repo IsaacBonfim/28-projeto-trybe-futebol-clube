c52fc4a78caa1f379e9ba1ba2a14390f20208615
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-21 16:12:06 -0300

    feat: Alterada a middleware de erro para que as informações requisitadas pela função ocupem a mesma linha.

--- app/backend/src/middleware/Error.Middleware.ts
+++ app/backend/src/middleware/Error.Middleware.ts
@@ -1,12 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import CodeError from '../errors/CodeError';
 
-const ErroMiddleware = (
-  err: CodeError,
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-) => {
+const ErroMiddleware = (err: CodeError, _req: Request, res: Response, _next: NextFunction) => {
   const { message, code } = err;
 
   res.status(code).json({ message });
