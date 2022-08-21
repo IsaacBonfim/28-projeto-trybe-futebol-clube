9c222a47ca204911fb5c1781a2e87806ff2e3460
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-21 16:17:16 -0300

    feat: Adicionada a camada de service a validação para email e senha, caso os campos estejam vazios ou incorretos. Enviando requisitos 5, 7, 9 e 11

--- app/backend/src/services/User.Service.ts
+++ app/backend/src/services/User.Service.ts
@@ -1,12 +1,35 @@
 import 'dotenv/config';
 import * as jwt from 'jsonwebtoken';
+import * as Joi from 'joi';
 import User from '../database/models/User.Model';
+import Login from '../interfaces/Interfaces';
+import CodeError from '../errors/CodeError';
 
 class UserService {
+  static async loginValidation(login: Login) {
+    const validation = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+
+    const { value, error } = await validation.validate(login);
+
+    if (error) {
+      throw new CodeError('All fields must be filled', 400);
+    }
+
+    return value;
+  }
+
   static async login(email: string) {
     const result = await User.findOne(
       { where: { email }, raw: true },
     );
+
+    if (!result) {
+      throw new CodeError('Incorrect email or password', 401);
+    }
+
     return result;
   }
 
