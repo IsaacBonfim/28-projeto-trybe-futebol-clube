642af296d3c57d0d9024860eb5764ef93dce8440
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-24 20:23:55 -0300

    Alterado o nome das variáveis que estavam como nome incorreto e adicionada a chamada da função para validação da senha no Controller

--- app/backend/src/controllers/User.Controller.ts
+++ app/backend/src/controllers/User.Controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from 'express';
-import Servise from '../services/User.Service';
+import Service from '../services/User.Service';
 
 class UserController {
   static async login(req: Request, res: Response) {
-    const { email } = await Servise.loginValidation(req.body);
-    const user = await Servise.login(email);
-    const token = await Servise.getToken(user);
+    const { email, password } = await Service.loginValidation(req.body);
+    const user = await Service.login(email);
+
+    await Service.passwordValidation(password, user.password);
+
+    const token = await Service.getToken(user);
 
     res.status(200).json({ token });
   }
 
   static async loginVerification(req: Request, res: Response) {
     const token = req.headers.authorization || 'tokenfake';
-    const { data: { email } } = await Servise.tokenVerification(token);
-    const { role } = await Servise.login(email);
+    const { data: { email } } = await Service.tokenVerification(token);
+    const { role } = await Service.login(email);
 
     res.status(200).json({ role });
   }
