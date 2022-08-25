887b6a0b017a3bbeac9fd290a2e065308e775689
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-25 16:52:29 -0300

    Ajuste na middleware de erro para retornar o código 500 como padrão caso um outro código não seja informado

--- app/backend/src/middleware/Error.Middleware.ts
+++ app/backend/src/middleware/Error.Middleware.ts
@@ -4,7 +4,7 @@ import CodeError from '../errors/CodeError';
 const ErroMiddleware = (err: CodeError, _req: Request, res: Response, _next: NextFunction) => {
   const { message, code } = err;
 
-  res.status(code).json({ message });
+  res.status(code || 500).json({ message });
 };
 
 export default ErroMiddleware;
